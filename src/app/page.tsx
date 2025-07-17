cd2cbe96544e910983212b1fa7d23b354ddb980e
Tech Know <techknow237@gmail.com>
2025-07-17 12:09:20 +0000

    this link leads to the logo. use it https://sdmntpraustraliaeast.oaiuser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function WelcomePage() {
     <div className="flex flex-col min-h-screen bg-background">
       <header className="p-4 flex justify-between items-center">
         <div className="flex items-center gap-2">
-            <Image src="https://firebasestudio.app/assets/images/CamerPay/125861b3-469f-43b9-a417-184065671151.png" alt="CamerPay Logo" width={40} height={40} />
+            <Image src="https://sdmntpraustraliaeast.oaiusercontent.com/files/00000000-4234-61fa-a3ae-41bb358dfb59/raw?se=2025-07-17T12%3A58%3A11Z&sp=r&sv=2024-08-04&sr=b&scid=f90c823e-4c67-5c14-bf33-887a3053545c&skoid=f71d6506-3cac-498e-b62a-67f9228033a9&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-07-17T04%3A22%3A28Z&ske=2025-07-18T04%3A22%3A28Z&sks=b&skv=2024-08-04&sig=K6MwvCkHMjaG9mCChVJTOjrVaRtZ7am41bNrVZgq1CM%3D" alt="CamerPay Logo" width={40} height={40} />
             <h1 className="text-2xl font-bold text-primary">CamerPay</h1>
         </div>
         <div>
